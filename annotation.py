2f23155f39e3a991aa3b46571ea858edbf2435d4
Marijn Koolen <marijn.koolen@gmail.com>
2017-03-03 12:49:46 +0100

    Add status_code to InvalidAnnotation error

--- annotation.py
+++ annotation.py
@@ -194,16 +194,16 @@ class AnnotationStore(object):
 class InvalidAnnotation(Exception):
     status_code = 400
 
-    def __init__(self, message, status_code=None, payload=None):
+    def __init__(self, message, status_code=400, payload=None):
         Exception.__init__(self)
         self.message = message
-        if status_code is not None:
-            self.status_code = status_code
+        self.status_code = status_code
         self.payload = payload
 
     def to_dict(self):
         rv = dict(self.payload or ())
         rv['message'] = self.message
+        rv['status_code'] = self.status_code
         return rv
 
 class AnnotationExistsError(Exception):
