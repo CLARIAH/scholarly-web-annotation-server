f03db49f9db0f1fdc9bce54dab39dff607fec4d7
Marijn Koolen <marijn.koolen@gmail.com>
2017-04-03 15:14:24 +0200

    Add resource type validation, more informative resource representation and registration information

--- resource_model.py
+++ resource_model.py
@@ -12,13 +12,18 @@ class Resource(object):
         self.subresources = {}
 
     def __repr__(self):
-        rv = dict(self.subresources)
-        rv['resource'] = self.resource
-        rv['typeof'] = self.typeof
-        return json.dumps(rv)
+        rv = "%s(%s, %s)\n" % (self.__class__.__name__, self.typeof, self.resource)
+        for relation_type in self.get_relations():
+            rv += "  %s\n"  % (relation_type)
+            for subresource in self.get_subresources(relation_type):
+                rv += "    %s(%s, %s)\n" % (subresource.__class__.__name__, subresource.typeof, subresource.resource)
+        return rv
 
     def id(self):
         return self.resource
+
+    def type(self):
+        return self.typeof
 
     def validate(self, resource_map):
         message = None
@@ -54,13 +59,16 @@ class ResourceStore(object):
 
     def register_by_map(self, resource_map):
         resource = self.parse_resource(resource_map)
-        self.register_resource(resource)
+        response = {"ignored": [], "registered": []}
+        self.register_resource(resource, response)
+        return response
 
-    def register_resource(self, resource):
-        self.index_resource(resource)
+    def register_resource(self, resource, response):
+        status = self.index_resource(resource)
+        response[status] += [resource.id()]
         for relation_type in resource.get_relations():
             for subresource in resource.get_subresources(relation_type):
-                self.register_resource(subresource)
+                self.register_resource(subresource, response)
 
     def has_resource(self, resource_id):
         return resource_id in self.resource_index.keys()
@@ -70,29 +78,48 @@ class ResourceStore(object):
             return self.resource_index[resource_id]
         return None
 
+    def get_resource_type(self, resource_id):
+        if self.has_resource(resource_id):
+            return self.resource_index[resource_id].type()
+
     def index_resource(self, resource):
+        status = "ignored"
         # check if resource is already known
         if resource.id() not in self.resource_index:
             self.resource_index[resource.id()] = resource
+            status = "registered"
+        registered_type = self.get_resource_type(resource.id())
+        if  registered_type != resource.type():
+            message = "Conflicting resource types: resource %s is already registered as type %s and cannot be additionally registered as type %s" % (resource.id(), registered_type, resource.type())
+            raise InvalidResourceMapError(message)
+        return status
 
     def parse_resource(self, resource_map):
         self.check_required_properties(resource_map)
-        self.check_resource_is_IRI(resource_map)
         self.vocab_store.register_vocabulary(resource_map['vocab'])
+        self.check_resource_is_IRI(resource_map["resource"])
+        self.check_resource_type_is_valid(resource_map["typeof"])
         subresources = self.get_subresources(resource_map)
         resource = Resource(resource_map)
         resource.add_subresources(subresources)
         return resource
 
+    def check_resource_type_is_valid(self, resource_type):
+        resource_class = self.vocab_store.lookupLabel(resource_type)
+        if not resource_class or not self.vocab_store.is_class(resource_class):
+            message="Illegal resource type: %s" % (resource_type)
+            raise InvalidResourceMapError(message)
+
     def is_relation(self, label, resource_map):
         if label in self.required:
             return False # skip own properties
         subject = self.vocab_store.lookupLabel(label)
-        # check if subject belongs to vocabulary
-        #if resource_map['vocab'] not subject.toPython()
+        # check if subject belongs to vocabulary specified in resource map
+        # TO DO
         # check if subject is relation
         if not subject or not self.vocab_store.is_property(subject):
-            raise InvalidResourceMapError(message="Illegal resource relationship: %s" % (label))
+            message="Illegal resource relationship: %s" % (label)
+            raise InvalidResourceMapError(message)
         return True
 
     def get_subresources(self, resource_map):
@@ -113,11 +140,11 @@ class ResourceStore(object):
             message = 'resource is missing the following properties: %s' % (missing)
             raise InvalidResourceMapError(message)
 
-    def check_resource_is_IRI(self, resource_map):
+    def check_resource_is_IRI(self, resource_id):
         try:
-            parse_IRI(resource_map['resource'])
+            parse_IRI(resource_id)
         except ValueError:
-            raise InvalidResourceMapError(message = 'resource identifier is not an IRI: %s' % (resource_map['resource']))
+            raise InvalidResourceMapError(message = 'resource identifier is not an IRI: %s' % (resource_id))
 
 class InvalidResourceMapError(Exception):
     status_code = 400
