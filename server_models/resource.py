a4990b398df8efc0c73b279dd47ff58532811b11
Marijn Koolen <marijn.koolen@gmail.com>
2017-04-07 15:42:07 +0200

    Add resource map generation function

--- server_models/resource.py
+++ server_models/resource.py
@@ -7,34 +7,33 @@ class Resource(object):
 
     def __init__(self, resource_map=None):
         self.validate(resource_map)
-        self.resource = resource_map['resource']
-        self.typeof = resource_map['typeof']
+        self.id = resource_map["id"]
+        self.type = resource_map["type"]
+        self.vocab = resource_map["vocab"]
         self.subresources = defaultdict(list)
 
     def __repr__(self):
-        rv = "%s(%s, %s)\n" % (self.__class__.__name__, self.typeof, self.resource)
+        rv = "%s(%s, %s)\n" % (self.__class__.__name__, self.type, self.id)
+        rv += "@context: %s\n" % (self.vocab)
         for relation_type in self.get_relations():
             rv += "  %s\n"  % (relation_type)
             for subresource in self.get_subresources(relation_type):
-                rv += "    %s(%s, %s)\n" % (subresource.__class__.__name__, subresource.typeof, subresource.resource)
+                rv += "    %s(%s, %s)\n" % (subresource.__class__.__name__, subresource.type, subresource.id)
         return rv
 
-    def id(self):
-        return self.resource
-
-    def type(self):
-        return self.typeof
-
     def validate(self, resource_map):
+        required = ["id", "type", "vocab"]
         message = None
         if resource_map == None:
             message="Resources should be initialized by a resource map"
+            raise ResourceError(message)
         elif type(resource_map) != dict:
-            message="Resources MUST be an object with 'resource' and 'typeof' properties"
-        elif 'resource' not in resource_map or 'typeof' not in resource_map:
-            message="Resource maps MUST have 'resource' and 'typeof' properties"
-        if message:
-            raise InvalidResourceError(message)
+            message="Resources MUST be an object with 'id', 'type' and 'vocab' properties"
+            raise ResourceError(message)
+        for property in required:
+            if property not in resource_map.keys():
+                message="Resource maps MUST have 'id' and 'type' properties"
+                raise ResourceError(message)
 
     def add_subresources(self, subresources, relation_type=None):
         if relation_type:
@@ -48,7 +47,7 @@ class Resource(object):
         if isinstance(subresource, Resource):
             self.subresources[relation_type] += [subresource]
         else:
-            raise(InvalidResourceError(message="subresource is not a Resource object"))
+            raise(ResourceError(message="subresource is not a Resource object"))
 
     def get_subresources(self, relation_type=None):
         if relation_type == None:
@@ -62,8 +61,8 @@ class Resource(object):
 
     def list_members(self, requested_type=None):
         members = []
-        if not requested_type or self.typeof == requested_type:
-            members += [self.resource]
+        if not requested_type or self.type == requested_type:
+            members += [self.id]
         for relation_type in self.subresources.keys():
             if requested_type and relation_type != requested_type:
                 continue
@@ -75,7 +74,7 @@ class ResourceStore(object):
 
     def __init__(self, config):
         self.resource_file = config['resource_file']
-        self.required = ["typeof", "vocab", "resource"]
+        self.required = ["type", "vocab", "id"]
         self.vocab_store = VocabularyStore(config)
         self.resource_index = {}
         self.load_index()
@@ -90,7 +89,7 @@ class ResourceStore(object):
 
     def get_resource_type(self, resource_id):
         if self.has_resource(resource_id):
-            return self.resource_index[resource_id].type()
+            return self.resource_index[resource_id].type
 
     def register_by_map(self, resource_map):
         resource = self.parse_resource_map(resource_map)
@@ -100,25 +99,43 @@ class ResourceStore(object):
 
     def register_resource(self, resource, response):
         status = self.index_resource(resource)
-        response[status] += [resource.id()]
+        response[status] += [resource.id]
         for relation_type in resource.get_relations():
             for subresource in resource.get_subresources(relation_type):
                 self.register_resource(subresource, response)
 
     def index_resource(self, resource):
         # check if resource is already known
-        if self.is_registered(resource.id()):
-            self.check_type_consistent(resource.id(), resource.type())
+        if self.has_resource(resource.id):
+            self.check_type_consistent(resource.id, resource.type)
             return "ignored"
         else:
-            self.resource_index[resource.id()] = resource
+            self.resource_index[resource.id] = resource
             return "registered"
+
+    def generate_resource_map(self, resource_id):
+        if not self.has_resource(resource_id):
+            raise ResourceError(message="Unknown resource id")
+        resource = self.get_resource(resource_id)
+        resource_map = {
+            "id": resource.id,
+            "type": resource.type,
+            "vocab": resource.vocab
+        }
+        for relation_type in resource.get_relations():
+            resource_map[relation_type] = []
+            for subresource in resource.get_subresources(relation_type):
+                subresource_map = self.generate_resource_map(subresource.id)
+                if subresource_map["vocab"] == resource.vocab:
+                    del(subresource_map["vocab"])
+                resource_map[relation_type] += [subresource_map]
+        return resource_map
 
     def parse_resource_map(self, resource_map):
         self.check_required_properties(resource_map)
         self.vocab_store.register_vocabulary(resource_map['vocab'])
-        self.check_resource_is_IRI(resource_map["resource"])
-        self.check_resource_type_is_valid(resource_map["typeof"])
+        self.check_resource_id_is_IRI(resource_map["id"])
+        self.check_resource_type_is_valid(resource_map["type"])
         resource = self.make_resource(resource_map)
         subresources = self.parse_subresources(resource_map)
         resource.add_subresources(subresources)
@@ -128,35 +145,32 @@ class ResourceStore(object):
         missing = ", ".join([key for key in self.required if key not in resource_map.keys()])
         if missing:
             message = 'resource is missing the following properties: %s' % (missing)
-            raise InvalidResourceMapError(message)
+            raise ResourceError(message)
 
-    def check_resource_is_IRI(self, resource_id):
+    def check_resource_id_is_IRI(self, resource_id):
         try:
             parse_IRI(resource_id)
         except ValueError:
-            raise InvalidResourceMapError(message = 'resource identifier is not an IRI: %s' % (resource_id))
+            raise ResourceError(message = 'resource identifier is not an IRI: %s' % (resource_id))
 
     def check_resource_type_is_valid(self, resource_type):
         resource_class = self.vocab_store.lookupLabel(resource_type)
         if not resource_class or not self.vocab_store.is_class(resource_class):
             message="Illegal resource type: %s" % (resource_type)
-            raise InvalidResourceMapError(message)
+            raise ResourceError(message)
 
     def make_resource(self, resource_map):
-        if self.is_registered(resource_map["resource"]):
-            self.check_type_consistent(resource_map["resource"], resource_map["typeof"])
-            return self.get_resource(resource_map["resource"])
+        if self.has_resource(resource_map["id"]):
+            self.check_type_consistent(resource_map["id"], resource_map["type"])
+            return self.get_resource(resource_map["id"])
         else: # only make a new resource when id is not previously registered
             return Resource(resource_map)
-
-    def is_registered(self, resource_id):
-        return resource_id in self.resource_index
 
     def check_type_consistent(self, resource_id, resource_type):
         registered_type = self.get_resource_type(resource_id)
         if  registered_type != resource_type:
             message = "Conflicting resource types: resource %s is already registered as type %s and cannot be additionally registered as type %s" % (resource_id, registered_type, resource_type)
-            raise InvalidResourceMapError(message)
+            raise ResourceError(message)
 
     def parse_subresources(self, resource_map):
         subresources = defaultdict(list)
@@ -176,7 +190,7 @@ class ResourceStore(object):
         # check if subject is relation
         if not subject or not self.vocab_store.is_property(subject):
             message="Illegal resource relationship: %s" % (label)
-            raise InvalidResourceMapError(message)
+            raise ResourceError(message)
         return True
 
     def get_resource_relations(self, resource_map):
@@ -195,22 +209,7 @@ class ResourceStore(object):
         except (FileNotFoundError, EOFError):
             pass
 
-class InvalidResourceMapError(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=400, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        rv['status_code'] = self.status_code
-        return rv
-
-class InvalidResourceError(Exception):
+class ResourceError(Exception):
     status_code = 400
 
     def __init__(self, message, status_code=400, payload=None):
